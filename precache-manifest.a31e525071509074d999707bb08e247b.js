6816ed3d6d1213de5e3273c8a23d17d92a17f5ad
Bence Házi <hazibence1109@gmail.com>
2020-05-25 22:22:26 +0200

    Updates

---
+++ precache-manifest.a31e525071509074d999707bb08e247b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475404e4c9741823cc1284be8a47238f",
+    "url": "/github.io/react-typingspeedcalculator/index.html"
+  },
+  {
+    "revision": "c02ae783f9c9dbc95d43",
+    "url": "/github.io/react-typingspeedcalculator/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a38e9617bb7ae0c67141",
+    "url": "/github.io/react-typingspeedcalculator/static/js/2.0d3c7948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-typingspeedcalculator/static/js/2.0d3c7948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02ae783f9c9dbc95d43",
+    "url": "/github.io/react-typingspeedcalculator/static/js/main.9cd5f9aa.chunk.js"
+  },
+  {
+    "revision": "da79795fbfa65978ef65",
+    "url": "/github.io/react-typingspeedcalculator/static/js/runtime-main.d8ea0573.js"
+  }
+]);
